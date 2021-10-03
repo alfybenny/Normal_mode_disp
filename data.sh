75374ad4d65660616ded57821406038ffc00ebd5
alfybenny <ab8483@princeton.edu>
2021-10-03 17:25:26 -0400

    Command line variables to data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+<< COMMAND_LINE_VARIABLES 
+	* Adding command line variables
+	* i: input file
+	* n: Desired frequency mode
+COMMAND_LINE_VARIABLES
+
+while getopts i:n: flag
+do
+    case "${flag}" in
+        i) logfile=${OPTARG};;
+        n) nthfreq=${OPTARG};; #Still not active
+    esac
+done
+
+
 # Gaussian log file
-file="py101_opt.log"
+file=${logfile} #"py101_opt.log"
 
 # No. of atoms
 Na=42
@@ -9,7 +24,10 @@ Na=42
 # Frequency data from Gaussian
   # The above part (Number of frequency, Symmetry, Red. masses, IR Inten)
 grep -r -i -B 5 "IR Inten    --" ${file} > freq_header.txt
-  # The eigenvector (x, y, z for Na atoms)
+
+# The eigenvectors (dx, dy, dz) for each frequency modes
 grep -r -i -A $Na "IR Inten    --" ${file} > freq_eigvec_1.txt
 sed '/IR Inten    --/d' freq_eigvec_1.txt > freq_eigvec.txt
 rm freq_eigvec_1.txt
+
+
