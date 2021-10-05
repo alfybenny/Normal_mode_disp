b04215d3037ce5bf62332be5eda48dc7d30a5449
alfybenny <ab8483@princeton.edu>
2021-10-05 12:22:54 -0400

    partially working data.sh for n > 3 and n not divisible by 3

--- data.sh
+++ data.sh
@@ -31,9 +31,36 @@ sed '/IR Inten    --/d' freq_eigvec_1.txt > freq_eigvec.txt
 rm freq_eigvec_1.txt
 
 # Grepping till the ${nthfreq} with each one having ${Na} atoms and tailing the last ${Na} sets of data\
-# Source "https://unix.stackexchange.com/questions/115224/print-line-after-nth-occurrence-of-a-match"
-grep -A ${Na} -m ${nthfreq} 'Atom' freq_eigvec.txt | tail -n ${Na} > ${nthfreq}freq.log 
+# SOURCE "https://unix.stackexchange.com/questions/115224/print-line-after-nth-occurrence-of-a-match"
 
-# WARNING!!! Code is incomplete. ${nthfreq} is only printing the set of eigenvectors in the gaussian log file. 
-# Need to awk the x y z as well
-# A problem for another day
+	# WARNING: Only if nthfreq > 3 and nthfreq is not divisible by 3-------------------------------------------------------
+
+
+	# The following circus is because of the shitty log file gaussian gives
+	# Note that gaussian prints freqencies as 3 sets of coloumns
+
+div=$(expr ${nthfreq} / 3)
+row=$(expr ${div} + 1)
+mult=$(expr 3 \* ${div})
+coloumn=$(expr ${nthfreq} % ${mult})
+
+	# Getting the required row of 3 sets of eigenvectors from gaussian
+grep -A ${Na} -m ${row} 'Atom' freq_eigvec.txt | tail -n ${Na} > ${row}section.txt 
+
+	# Isolating the eigenvectors of nth frequency
+awk1=$(expr ${coloumn} + 2)
+awk2=$(expr ${coloumn} + 3)
+awk3=$(expr ${coloumn} + 4)
+
+# SOURCE: "https://stackoverflow.com/questions/19075671/how-do-i-use-shell-variables-in-an-awk-script"
+awk -v a="$awk1" -v b="$awk2" -v c="$awk3" '{print $a " " $b " " $c " "}' ${row}section.txt > ${nthfreq}.log
+
+
+rm *txt
+
+# NOTE FOR THE NEXT DAY: Now the code only works for freqeuncies which are greater than 3 and is not divisible by 3
+# Have to insert if loops and fix this shit
+# A problem for another time
+
+
+
